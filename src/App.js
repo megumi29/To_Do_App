ab328affa2f68248405b7e21c496c2d53ea5322a
megumi29 <megumisan90@gmail.com>
2020-08-27 14:46:05 -0400

    updated app.js file to reflect changes to my todo app and add images to src and public folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./images/logo.svg";
+import AppName from "./images/AppName.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <ul className="menu">
+            <li>Home</li>
+            <li>Upcoming To Dos</li>
+            <li>Create New To Do</li>
+            <li>Log Out</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <div className="logoimg">
+        <img src={AppName} alt="logo" />
+      </div>
+      <h2>To Do Today </h2>
+      <h3>Thurs, Aug 27, 2020</h3>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          Wash The Dog
+        </li>
+        <li>
+          <input type="checkbox" />
+          Get Groceries
+        </li>
+        <li>
+          <input type="checkbox" />
+          Finish Homework
+        </li>
+      </ul>
+    </>
   );
 }
 
