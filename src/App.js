67c800fbfe1bd700d2ea7ad1799e91c96ae00201
megumi29 <megumisan90@gmail.com>
2020-09-03 12:50:15 -0400

    added styles, avatar image, and jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
 // import logo from "./images/logo.svg";
 import AppName from "./images/AppName.png";
+import avatarImg from "./images/AuntPersona.jpg";
 import "./App.css";
+
+const user = {
+  username: "Jenna",
+  message: "Good Morning! What's On Your ToDos Today?",
+  avatarImg: avatarImg,
+};
+
+let toDoLists = [
+  {
+    id: 1,
+    title: "Errands To Run",
+    description: "Get Groceries",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Bills To Pay",
+    description: "Credit Card",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Chores",
+    description: "Do Laundry",
+    completed: false,
+  },
+];
 
 function App() {
   return (
@@ -20,22 +48,34 @@ function App() {
       <div className="logoimg">
         <img src={AppName} alt="logo" />
       </div>
-      <h2>To Do Today </h2>
-      <h3>Thurs, Aug 27, 2020</h3>
-      <ul>
-        <li>
-          <input type="checkbox" />
-          Wash The Dog
-        </li>
-        <li>
-          <input type="checkbox" />
-          Get Groceries
-        </li>
-        <li>
-          <input type="checkbox" />
-          Finish Homework
-        </li>
-      </ul>
+
+      <div className="user-section">
+        <img src={user.avatarImg} alt="users_avatar" />
+        <p>{user.username}</p>
+        <p>{user.message}</p>
+      </div>
+
+      <div className="ToDo_List">
+        <ul>
+          <h3>ToDos</h3>
+          <p className="date">Thursday, Sept 3, 2020</p>
+          {toDoLists.map((toDoList) => {
+            return (
+              <li>
+                <span>
+                  <p className="catergory">{toDoList.title}</p>
+                  <input
+                    className="checkboxes"
+                    type="checkbox"
+                    checked={toDoList.completed}
+                  />
+                  <p className="ToDo">{toDoList.description}</p>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
