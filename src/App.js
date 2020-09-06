9bc940b0a99b177b06e25edbbb5d79cb2f41c86a
megumi29 <megumisan90@gmail.com>
2020-09-06 06:21:07 -0400

    updated css styles in js and created a side nav bar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ let toDoLists = [
     id: 1,
     title: "Errands To Run",
     description: "Get Groceries",
-    completed: false,
+    completed: true,
   },
   {
     id: 2,
@@ -33,45 +33,55 @@ let toDoLists = [
 
 function App() {
   return (
-    <>
-      <header>
+    // Nav Section
+    <div style={pageContainer}>
+      <div style={navStyles.sideNavSection}>
+        <img
+          src={user.avatarImg}
+          style={navStyles.userSectionImg}
+          alt="users_avatar"
+        ></img>
+        <p style={navStyles.sideNavUserName}>{user.username}</p>
         <nav>
-          <ul className="menu">
-            <li>Home</li>
-            <li>Upcoming To Dos</li>
-            <li>Create New To Do</li>
+          <ul>
+            <li style={navStyles.navMenu}>Home</li>
+            <li style={navStyles.navMenu}>Upcoming To Dos</li>
+            <li style={navStyles.navMenu}>Create New To Do</li>
             <li>Log Out</li>
           </ul>
+          <div style={navStyles.logoImg}>
+            <img src={AppName} style={navStyles.imgSize} alt="logo" />
+          </div>
         </nav>
-      </header>
-
-      <div className="logoimg">
-        <img src={AppName} alt="logo" />
       </div>
 
-      <div className="user-section">
-        <img src={user.avatarImg} alt="users_avatar" />
-        <p style={userSection}>{user.username}</p>
-        <p style={userSectionMessage}>{user.message}</p>
-      </div>
-
+      {/* Todo List Section */}
       <div>
+        <div>
+          <p style={userSectionMessage}>{user.message}</p>
+        </div>
         <ul>
           <h3>ToDos</h3>
           <p className="date">Thursday, Sept 3, 2020</p>
           {toDoLists.map((toDoList) => {
             return (
-              <li style={listStyles}>
+              <li style={listStyles.list}>
                 {/* <span className="catergory">{toDoList.title}</span> */}
-                <div style={list}>
+                <div>
                   <input
-                    className="checkboxes"
+                    style={listStyles.checkBoxes}
                     type="checkbox"
                     checked={toDoList.completed}
                   />
                 </div>
 
-                <div style={list}>
+                <div
+                  style={
+                    toDoList.completed
+                      ? navStyles.toDoCompleted
+                      : navStyles.toDoDescription
+                  }
+                >
                   <span className="ToDo">{toDoList.description}</span>
                 </div>
               </li>
@@ -79,35 +89,82 @@ function App() {
           })}
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
-const listStyles = {
-  textAlign: "left",
+// Styles
+const pageContainer = {
   display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
+};
+const navStyles = {
+  sideNavSection: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: "100%",
+    padding: "1rem",
+    backgroundColor: "#b892ff",
+    fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+    fontWeight: "bold",
+  },
+  navMenu: {
+    color: "white",
+    marginRight: "1rem",
+    marginBottom: "2rem",
+  },
+  sideNavUserName: {
+    marginBottom: "3rem",
+    color: "#ffff",
+  },
+  userSectionImg: {
+    width: "10rem",
+    borderRadius: "50%",
+  },
+  logoImg: {
+    position: "absolute",
+    bottom: "1rem",
+  },
+  imgSize: {
+    width: "60%",
+  },
+  toDoCompleted: {
+    color: "#d8dbe2",
+    textDecoration: "line-through",
+  },
+  toDoDescription: {
+    fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+  },
 };
 
-const list = {
-  display: "flex",
-  flexDirection: "row",
-  margin: "0",
-  padding: ".5rem",
-  fontSize: "11px",
+const listStyles = {
+  list: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px solid #8e7dbe",
+    marginBottom: "2rem",
+    marginRight: "6rem",
+    marginLeft: "6rem",
+    borderRadius: "5rem",
+    boxShadow: "0 .2rem .5rem 0 #8e7dbe",
+  },
+  checkBoxes: {
+    display: "flex",
+    flexDirection: "row",
+    margin: ".5rem",
+    fontSize: "11px",
+  },
 };
 
 const userSectionMessage = {
   fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
   fontWeight: "bold",
   textAlign: "center",
-};
-
-const userSection = {
-  fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
-  fontWeight: "bold",
+  padding: "1rem",
 };
 
 export default App;
